b8f61d91bec2c551341d819cad802872bdf41e3f
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-12-21 13:32:17 +0100

    update models

--- rental/models.py
+++ rental/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 class Car(models.Model):
@@ -8,15 +9,22 @@ class Car(models.Model):
     description = models.TextField()
     daily_rent = models.IntegerField()
     is_available = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-created',)
 
     def get_absolute_url(self):
         return reverse('car-details', kwargs={'pk', self.pk})
 
     def __str__(self):
-        return self.name
+        return 'Car {}'.format(self.name)
 
 class Booking(models.Model):
-    customer_name = models.CharField(max_length=100)
+    customer = models.ForeignKey(User, related_name='cars')
     booking_start_date = models.DateTimeField()
     booking_end_date = models.DateTimeField()
     is_approved = models.BooleanField()
+    
+    def __str__(self):
+        return 'Book by {} on {}'.format(self.customer, self.booking_start_date)
