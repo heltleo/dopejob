57eabe312fcbf0036cd954d45905a52b8dcc3a4b
heltleo <heltleo59@gmail.com>
2017-12-25 18:52:03 +0100

    add mail backend

--- activcar/settings_production.py
+++ activcar/settings_production.py
@@ -11,3 +11,19 @@ ALLOWED_HOSTS = ['*',]
 DEBUG = False
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+ADMINS = (
+    ('Délita', 'delita.makanda@gmail.com'),
+)
+
+SEND_BROKEN_LINK_EMAILS=True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+SERVER_EMAIL = ''
+EMAIL_HOST = ''
+SERVER_EMAIL = EMAIL_HOST_USER
