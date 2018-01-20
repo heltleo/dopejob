96e1a4f0ad55cfc8dea5370ff89b7fbcc87b0013
heltleo <heltleo59@gmail.com>
2018-01-20 18:04:07 +0100

    add fields to models

--- rental/models.py
+++ rental/models.py
@@ -6,12 +6,39 @@ from django.utils import timezone
 # Create your models here.
 
 class Car(models.Model):
+    BERLINE = 'BE'
+    COUPE = 'CO'
+    CABRIOLET = 'CA'
+    ROADSTER = 'RA'
+    KATKAT = 'KA'
+    SUV = 'SU'
+    CROSSOVER = 'CR'
+    BREAK = 'BR'
+    MONOSPACE = 'MO'
+    CARS_MODEL_CHOICES = (
+        (BERLINE, 'Berline'),
+        (COUPE, 'Coupé'),
+        (CABRIOLET, 'Cabriolet'),
+        (ROADSTER, 'Roadster'),
+        (KATKAT, '4X4'),
+        (SUV, 'Suv'),
+        (CROSSOVER, 'Crossover'),
+        (BREAK, 'Break'),
+        (MONOSPACE, 'Monospace'),
+    )
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='car_images')
     description = models.TextField()
     daily_rent = models.IntegerField()
     is_available = models.BooleanField()
     created = models.DateTimeField(auto_now_add=True)
+    localization = models.CharField(max_length=100, blank=True, null=True)
+    car_models = models.CharField(
+        max_length=2,
+        choices=CARS_MODEL_CHOICES,
+        default=BERLINE,
+    )
+
 
     class Meta:
         ordering = ('-created',)
@@ -21,6 +48,9 @@ class Car(models.Model):
 
     def was_published_recently(self):
         return self.created >= timezone.now() - datetime.timedelta(days=1)
+
+    def is_upperclass(self):
+        return self.model in (self.BERLINE, self.CABRIOLET)
 
     def __str__(self):
         return 'Car {}'.format(self.name)
