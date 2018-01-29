e3d36b4035dfa7ac0c536cc7fd52928983a9070f
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-01-29 14:24:21 +0100

    hide help_text

--- rental/static/scripts/app.js
+++ rental/static/scripts/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('init');
+var prepareFormBase = function() {
+  var $form = $('body').find('form'),
+      $helpText = $form.find('helptext');
+  
+  $helpText.each(function(i) {
+    $(this).hide();
+  });
+};
+
+$(function() {
+  prepareFormBase();
+});
