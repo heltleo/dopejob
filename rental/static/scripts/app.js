9d77227220bdec19fc31fde3ce595c36511b20c4
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-02-13 11:27:36 +0100

    refactor code

--- rental/static/scripts/app.js
+++ rental/static/scripts/app.js
@@ -2,6 +2,10 @@
 
 function changeViewBySelect() {
     var page = $('#sort_by').val();
+    
+    $('#sort_by').on('change', function() {
+        location = this.options[this.selectedIndex].value;
+    });
 }
 
 $(function() {
