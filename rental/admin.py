41df925442c321f90cc748c256edadea2c8f8166
heltleo <heltleo59@gmail.com>
2018-05-12 15:56:33 +0200

    send email by admin

--- rental/admin.py
+++ rental/admin.py
@@ -16,6 +16,20 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ['customer__username']
     list_editable = ['is_approved']
 
+    actions = ['email_customers']
+
+    def email_customers(self, request, queryset):
+        for booking in queryset:
+            if booking.is_approved:
+                email_body = """Dear, {}, We are pleased to inform you that your booking has been approved. Thanks""".format(booking.customer.username)
+            else:
+                email_body = """Dear {}, Unfortunatly we do not have the capacity right now to accept your booking. Thanks""".format(booking.customer.username)
+
+            print(email_body)
+
+        self.message_user(request, 'Emails were send successfully')
+    email_customers.short_description = 'Send email about booking status to customers'
+
 admin.site.register(Car)
 admin.site.register(Booking, BookingAdmin)
 admin.site.register(Contact, ContactFormAdmin)
