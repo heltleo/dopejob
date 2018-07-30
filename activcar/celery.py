081ea930a8eec992b262ca63675e0dc5b05d833a
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-07-30 09:34:47 +0200

    Update celery.py

--- activcar/celery.py
+++ activcar/celery.py
@@ -8,3 +8,7 @@ app = Celery('rental')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+  print('Request: {0}'.format(self.request))
