291ed33ca2e4b0a12a902fd0d39611b7ec20de6f
heltleo <heltleo59@gmail.com>
2018-08-19 20:53:47 +0200

    humanize options

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.humanize',
     'dj_pagination',
     'storages',
     'accounts',
